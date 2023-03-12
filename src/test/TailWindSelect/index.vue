d45a553a1430e6ec970f9fa3cc9eb84952af60c5
cookingBird <86037230+cookingBird@users.noreply.github.com>
2023-03-12 23:58:11 +0800

    cross-env 容易引起包冲突

--- src/test/TailWindSelect/index.vue
+++ src/test/TailWindSelect/index.vue
@@ -1,23 +1,21 @@
 <template>
   <section class="TailWindSelect">
-    <el-input class=" el-input:rounded-none el-input:border-2 el-input:bg-red-500 el-input:border-blue-800">
-      hello world
+    <el-input
+      class="w-[200px] el-input:rounded-none el-input:border-2 el-input:bg-red-500 el-input:border-blue-800">
     </el-input>
   </section>
 </template>
 
-<style lang="css">
-
-</style>
+<style lang="css"></style>
 
 <script>
-export default {
-  name: 'TailWindSelect',
-  data () {
-    return {}
-  },
+  export default {
+    name: 'TailWindSelect',
+    data() {
+      return {};
+    },
 
-  methods: {},
-  mounted () { },
-}
+    methods: {},
+    mounted() {},
+  };
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,42 +1,42 @@
 /** @type {import('tailwindcss').Config} */
-const plugin = require('tailwindcss/plugin');
+const plugin = require('tailwindcss/plugin')
 module.exports = {
   mode: 'jit',
-  purge: ['./public/**/*.html', './src/**/*.{vue,less,css,scss,js,ts,jsx,tsx}'],
+  purge: ['./public/index.html', './src/**/*.{vue,less,css,scss,js,jsx}'],
   darkMode: false, // or 'media' or 'class'
   important: true,
   theme: {
-    extend: {},
+    extend: {}
   },
 
   plugins: [
     plugin(function ({ addVariant, e }) {
       addVariant('first-child', ({ modifySelectors, separator }) => {
         modifySelectors(({ className }) => {
-          return `.${e(`first-child${separator}${className}`)}:first-child`;
-        });
-      });
+          return `.${e(`first-child${separator}${className}`)}:first-child`
+        })
+      })
     }),
     plugin(function ({ addVariant, e }) {
       addVariant('light', ({ modifySelectors, separator }) => {
         modifySelectors(({ className }) => {
-          return `body.light .${e(`light${separator}${className}`)}`;
-        });
-      });
+          return `body.light .${e(`light${separator}${className}`)}`
+        })
+      })
     }),
     plugin(function ({ addVariant, e }) {
       addVariant('el-input', ({ modifySelectors, separator }) => {
         modifySelectors(({ className }) => {
-          return `.el-input${e(`${separator}${className}`)} .el-input__inner`;
-        });
-      });
+          return `.el-input${e(`${separator}${className}`)} .el-input__inner`
+        })
+      })
     }),
     plugin(function ({ addVariant, e }) {
       addVariant('small', ({ modifySelectors, separator }) => {
         modifySelectors(({ className }) => {
-          return `div[size=small].small${e(`${separator}${className}`)}`;
-        });
-      });
-    }),
-  ],
-};
+          return `div[size=small].small${e(`${separator}${className}`)}`
+        })
+      })
+    })
+  ]
+}
